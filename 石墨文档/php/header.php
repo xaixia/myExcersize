ce7721b9cfbf3ac95465071a5d892f088d4dd31d
xiaxia <786337828@qq.com>
2017-02-26 17:21:49 +0800

    上传石墨文档的练习代码

---
+++ 石墨文档/php/header.php
@@ -0,0 +1,18 @@
+<nav>
+        <div class="home-left left" >
+            <h4 class="left"></h4>
+            <p  class="left">我们正在招聘</p>
+        </div>
+        <ul class="home-right right">
+            <li class="active"><a href="index1.html">首页</a></li>
+            <li><a href="index2.html">个人版</a></li>
+            <li><a href="index3.html">企业版</a></li>
+            <li><a href="index4.html">定价</a></li>
+            <li><a href="index5.html">钉钉</a></li>
+            <li><a href="index6.html">下载APP</a></li>
+            <li><a href="index7.html">帮助</a></li>
+            <li><a href="#">登录</a></li>
+            <li><a href="#">注册</a></li>
+
+        </ul>
+    </nav>
